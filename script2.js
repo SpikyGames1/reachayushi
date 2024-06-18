2877e5770d2f3a4ffe1e5161af99b6ccf2ab7b5b
SpikyGames <167784765+SpikyGames1@users.noreply.github.com>
2024-06-18 14:51:04 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,3 +2,10 @@ const img = document.querySelector("img");
 img.ondragstart = () => {
   return false;
 };
+function isMobile() {
+  const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return regex.test(navigator.userAgent);
+}
+if (isMobile()) {
+  window.location.href(mobileVer/index.html);
+}
