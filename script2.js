d5c9fb4eed42e3f319eace574182eb1d37513708
SpikyGames <167784765+SpikyGames1@users.noreply.github.com>
2024-06-18 14:59:48 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -6,6 +6,16 @@ function isMobile() {
   const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
   return regex.test(navigator.userAgent);
 }
-if (isMobile()) {
-  window.location.href(mobileVer/index.html);
+function isMobileDevice() {
+  return /Mobi|Android/i.test(navigator.userAgent);
+}
+
+function isMobileDevice() {
+  const userAgent = navigator.userAgent;
+  const isMobile = /Mobi|Android/i.test(userAgent);
+  const isSmallScreen = window.innerWidth <= 768;
+  return isMobile || isSmallScreen;
+}
+if (isMobileDevice()) {
+  window.location.href = "mobileVer/index.html";
 }
